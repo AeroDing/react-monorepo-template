57b75aa6cb57f2466ad2048a1d6470a4a876f02a
dingpx <dingpx9999@gmail.com>
2025-03-18 23:56:14 +0800

    更新 ESLint 配置以支持 React，添加相关 ESLint 插件和依赖，升级 react-router-dom 版本，新增 @ant-design/icons 依赖，优化 pnpm 工作区配置。

--- app/app-site/vite.config.ts
+++ app/app-site/vite.config.ts
@@ -1,15 +1,18 @@
+import react from '@vitejs/plugin-react'
+import UnoCSS from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), AutoImport({
+  plugins: [tsconfigPaths(), react(), UnoCSS(), AutoImport({
     imports: [
       'react',
       'react-router-dom',
     ],
   })],
   server: {
+    host: true,
     port: 3000,
   },
   esbuild: {
