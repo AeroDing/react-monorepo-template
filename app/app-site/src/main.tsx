f236d019dfee40f71d61b8acf63126fa12cfbe57
AeroDing <dingpx9999@gmail.com>
2025-04-18 22:03:52 +0800

    feat(styles): 引入 uno.css 以增强样式支持
    在 main.tsx 文件中新增对 'virtual:uno.css' 的导入，以便于使用 UnoCSS 提供的样式功能，提升项目的样式管理和灵活性。

--- app/app-site/src/main.tsx
+++ app/app-site/src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { routes } from './router/routes'
 import './index.css'
+import 'virtual:uno.css'
 
 // 创建路由器
 const router = createBrowserRouter(routes)
